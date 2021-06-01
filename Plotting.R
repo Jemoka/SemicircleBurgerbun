64d5ebc61ae7597066ea9d4b6c514a0b825cc783
Houjun Liu <hliu@shabang.cf>
2021-06-01 10:53:15 -0700

    commit

--- Plotting.R
+++ Plotting.R
@@ -1,33 +1,35 @@
 library("tidyverse")
 library("plotly")
 
-step_size <- 0.1
-semicircle_fidelity <- 0.01
-max_rg <- 9
+fill_shape <- FALSE
+step_size <- 1
+crosssection_fidelity <- 0.1
+max_range <- 30
+tracefunc <- function(x) {
+    sqrt(3*x**2+12)
+}
+crossfunc <- function(x, y_range) {
+    rad <- tracefunc(x)/2
+    sapply(y_range, (function (x) sqrt(rad**2-(x-rad)**2)))
+}
 
-xass <- c(0:(max_rg/step_size)*step_size)
-yass <- sapply(xass, sqrt)
-
-#make_semicircle <- function(x) {
-    #tv <- sqrt(x)
-    #th <- tv/2
-    #c(tv, th)
-#}
+xass <- c(0:(max_range/step_size)*step_size)
+yass <- sapply(xass, tracefunc)
 
 fig <- plot_ly(x=xass, y=yass, z=0, type='scatter3d', mode="lines")
-fig <- fig %>% add_trace(x=9)
+fig <- fig %>% add_trace(x=max_range)
 fig <- fig %>% add_trace(y=0)
 
-for (x in 0:(max_rg/step_size)*step_size) {
-    y_group <- c(0:(((sqrt(x)/(semicircle_fidelity))))*semicircle_fidelity)
-    rad <- sqrt(x)/2
-    z_group <- sapply(y_group, (function (x) sqrt(rad**2-(x-rad)**2)))
+for (x in 0:(max_range/step_size)*step_size) {
+    y_group <- c(0:(((tracefunc(x)/(crosssection_fidelity))))*crosssection_fidelity)
+    z_group <- crossfunc(x, y_group)
 
-    fig <- fig %>% add_trace(x=x, y=y_group, z=z_group, type="scatter3d", mode="lines", surfaceaxis=0)
+    if (fill_shape)
+        fig <- fig %>% add_trace(x=x, y=y_group, z=z_group, type="scatter3d", mode="lines", surfaceaxis=0)
+    else
+        fig <- fig %>% add_trace(x=x, y=y_group, z=z_group, type="scatter3d", mode="lines")
 }
 
 fig
-
-
 
 
